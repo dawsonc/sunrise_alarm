b18c78f85e2dd8a2c201fd5cdb4c75dc5c859b73
Charles Dawson <charles.dwsn@gmail.com>
2023-09-09 16:42:32 -0400

    Fix install script

--- sunrise_alarm/install_alarm.sh
+++ sunrise_alarm/install_alarm.sh
@@ -7,9 +7,9 @@ mkdir -p "$HOME/.sunrise_alarm"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Add a cron job to run reenable_alarm.sh every day at noon (12:00 PM)
-(crontab -l ; echo "0 12 * * * $script_dir/reenable_alarm.sh $HOME/.sunrise_alarm") | crontab -
+(crontab -l ; echo "0 12 * * * $script_dir/reenable_alarm.sh $HOME/.sunrise_alarm >> $HOME/.sunrise_alarm/logfile 2>&1") | crontab -
 
 # Add a cron job to run alarm_exec.py every 10 seconds
-(crontab -l ; echo "* * * * * for i in {1..6}; do $script_dir/alarm_exec.py $HOME/.sunrise_alarm & sleep 10; done") | crontab -
+(crontab -l ; echo "* * * * * for i in {1..6}; do $script_dir/alarm_exec.py $HOME/.sunrise_alarm  >> $HOME/.sunrise_alarm/logfile 2>&1 & sleep 10; done") | crontab -
 
 echo "Cron jobs added successfully."
