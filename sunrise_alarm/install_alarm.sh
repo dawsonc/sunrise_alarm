a26d909eabccff8d842a274353cdc7c33c1e83f7
Charles Dawson <charles.dwsn@gmail.com>
2023-09-09 16:40:30 -0400

    Fix install script

--- sunrise_alarm/install_alarm.sh
+++ sunrise_alarm/install_alarm.sh
@@ -6,8 +6,6 @@ mkdir -p "$HOME/.sunrise_alarm"
 # Get the directory where this script is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Make a settings directory if it doesn't exist
-
 # Add a cron job to run reenable_alarm.sh every day at noon (12:00 PM)
 (crontab -l ; echo "0 12 * * * $script_dir/reenable_alarm.sh $HOME/.sunrise_alarm") | crontab -
 
