5628bdce26c35c39f32853695fbe79a82fa091ee
Charles Dawson <charles.dwsn@gmail.com>
2023-09-09 17:03:24 -0400

    Add logging

--- sunrise_alarm/alarm_exec.py
+++ sunrise_alarm/alarm_exec.py
@@ -21,19 +21,23 @@ def main(leds: LEDInterface, settings_dir: str):
     settings = load_settings(settings_dir)
     if check_disabled(settings_dir):
         # If alarm is disabled, turn off LEDs and exit
+        print("[alarm_exec] Alarm is disabled.")
         leds.set((0, 0, 0), 0)
     else:
         current_time = datetime.now().time()
 
         if before(current_time, settings.start_time):
             # If it's before the turn-on time, turn off LEDs
+            print(f"[alarm_exec] Current time {current_time} before the turn-on time {settings.start_time}.")
             leds.set((0, 0, 0), 0)
         elif before(current_time, settings.max_brightness_time):
             # If it's between turn-on and max-brightness times, set LEDs to appropriate brightness
             desired_intensity = get_desired_intensity(current_time, settings)
+            print(f"[alarm_exec] Current time {current_time}; setting LEDs to {desired_intensity}")
             leds.set(settings.color, desired_intensity)
         else:
             # If it's after the max-brightness time, set LEDs to full brightness
+            print(f"[alarm_exec] Current time {current_time}; setting LEDs to {settings.intensity}")
             leds.set(settings.color, settings.intensity)
 
 
